32cee82d7bb7e2b00b1f244f71ef8b6c89b85f27
Artur Peixoto Lucas <arturpeixoto71@gmail.com>
2024-08-07 11:40:40 -0300

    feat models and schemas

---
+++ madr/models.py
@@ -0,0 +1,75 @@
+from datetime import datetime
+
+from sqlalchemy import ForeignKey, func
+from sqlalchemy.orm import Mapped, mapped_column, registry, relationship
+
+table_registry = registry()
+
+
+@table_registry.mapped_as_dataclass
+class User:
+    __tablename__ = 'users'
+
+    id: Mapped[int] = mapped_column(init=False, primary_key=True)
+    username: Mapped[str] = mapped_column(unique=True)
+    password: Mapped[str]
+    email: Mapped[str] = mapped_column(unique=True)
+    created_at: Mapped[datetime] = mapped_column(
+        init=False, server_default=func.now()
+    )
+    updated_at: Mapped[datetime] = mapped_column(
+        init=False, server_default=func.now(), onupdate=func.now()
+    )
+
+    books: Mapped[list['Book']] = relationship(
+        init=False, back_populates='user', cascade='all, delete-orphan'
+    )
+    authors: Mapped[list['Author']] = relationship(
+        init=False, back_populates='user', cascade='all, delete-orphan'
+    )
+
+
+@table_registry.mapped_as_dataclass
+class Book:
+    __tablename__ = 'books'
+
+    id: Mapped[int] = mapped_column(init=False, primary_key=True)
+    title: Mapped[str]
+    year: Mapped[int]
+    author_id: Mapped[int] = mapped_column(
+        ForeignKey('authors.id', ondelete='CASCADE')
+    )
+    created_by_user: Mapped[int] = mapped_column(
+        ForeignKey('users.id', ondelete='SET NULL')
+    )
+    created_at: Mapped[datetime] = mapped_column(
+        init=False, server_default=func.now()
+    )
+    updated_at: Mapped[datetime] = mapped_column(
+        init=False, server_default=func.now(), onupdate=func.now()
+    )
+
+    user: Mapped[User] = relationship(init=False, back_populates='books')
+    author: Mapped['Author'] = relationship(init=False, back_populates='books')
+
+
+@table_registry.mapped_as_dataclass
+class Author:
+    __tablename__ = 'authors'
+
+    id: Mapped[int] = mapped_column(init=False, primary_key=True)
+    name: Mapped[str]
+    created_by_user: Mapped[int] = mapped_column(
+        ForeignKey('users.id', ondelete='SET NULL')
+    )
+    created_at: Mapped[datetime] = mapped_column(
+        init=False, server_default=func.now()
+    )
+    updated_at: Mapped[datetime] = mapped_column(
+        init=False, server_default=func.now(), onupdate=func.now()
+    )
+
+    user: Mapped[User] = relationship(init=False, back_populates='authors')
+    books: Mapped[list[Book]] = relationship(
+        init=False, back_populates='author'
+    )
---
+++ madr/schemas.py
@@ -0,0 +1,72 @@
+from datetime import datetime
+
+from pydantic import BaseModel, EmailStr
+
+
+class Message(BaseModel):
+    message: str
+
+
+class UserSchema(BaseModel):
+    username: str
+    email: EmailStr
+    password: str
+
+
+class UserPublic(BaseModel):
+    id: int
+    username: str
+    email: EmailStr
+
+
+class UserList(BaseModel):
+    users: list[UserPublic]
+
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str
+
+
+class TokenData(BaseModel):
+    username: str | None = None
+
+
+class BookSchema(BaseModel):
+    title: str
+    year: int
+    author_id: int
+
+
+class BookPublic(BookSchema):
+    id: int
+    created_at: datetime
+    updated_at: datetime
+
+
+class BookList(BaseModel):
+    books: list[BookPublic]
+
+
+class BookUpdate(BaseModel):
+    title: str | None = None
+    year: int | None = None
+    author_id: int | None = None
+
+
+class AuthorSchema(BaseModel):
+    name: str
+
+
+class AuthorPublic(AuthorSchema):
+    id: int
+    created_at: datetime
+    updated_at: datetime
+
+
+class AuthorList(BaseModel):
+    authors: list[AuthorPublic]
+
+
+class AuthorUpdate(BaseModel):
+    name: str | None = None
