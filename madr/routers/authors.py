4f7a20d18032f64d74567aec839433ba58d8f84c
Artur Peixoto Lucas <arturpeixoto71@gmail.com>
2024-08-07 12:01:51 -0300

    feat books, authors and users routes

---
+++ madr/routers/authors.py
@@ -0,0 +1,99 @@
+from http import HTTPStatus
+from typing import Annotated
+
+from fastapi import APIRouter, Depends, Query
+from fastapi.exceptions import HTTPException
+from sqlalchemy import select
+from sqlalchemy.orm import Session
+
+from madr.database import get_session
+from madr.models import Author, User
+from madr.schemas import (
+    Message,
+    AuthorList,
+    AuthorPublic,
+    AuthorSchema,
+    AuthorUpdate,
+)
+from madr.security import get_current_user
+
+router = APIRouter(prefix='/authors', tags=['authors'])
+T_Session = Annotated[Session, Depends(get_session)]
+T_CurrentUser = Annotated[User, Depends(get_current_user)]
+
+
+@router.post('/', status_code=HTTPStatus.CREATED, response_model=AuthorPublic)
+def create_author(
+    user: T_CurrentUser,
+    author: AuthorSchema,
+    session: T_Session,
+):
+    db_author = Author(
+        name=author.name,
+        created_by_user=user.id
+    )
+    session.add(db_author)
+    session.commit()
+    session.refresh(db_author)
+
+    return db_author
+
+
+@router.get('/', response_model=AuthorList)
+def list_authors(
+    session: T_Session,
+    user: T_CurrentUser,
+    name: str = Query(None),
+    offset: int = Query(None),
+    limit: int = Query(None),
+):
+    query = select(Author).where(Author.created_by_user == user.id)
+
+    if name:
+        query = query.filter(Author.name.contains(name))
+
+    authors = session.scalars(query.offset(offset).limit(limit)).all()
+
+    return {'authors': authors}
+
+
+@router.patch(
+    '/{author_id}', status_code=HTTPStatus.OK, response_model=AuthorPublic
+)
+def update_author(
+    author_id: int, user: T_CurrentUser, session: T_Session, author: AuthorUpdate
+):
+    db_author = session.scalar(
+        select(Author).where(Author.created_by_user == user.id, Author.id == author_id)
+    )
+
+    if not db_author:
+        raise HTTPException(
+            status_code=HTTPStatus.NOT_FOUND, detail='Author not found'
+        )
+
+    for key, value in author.model_dump(exclude_unset=True).items():
+        setattr(db_author, key, value)
+
+    session.add(db_author)
+    session.commit()
+    session.refresh(db_author)
+
+    return db_author
+
+
+@router.delete('/{author_id}', response_model=Message, status_code=HTTPStatus.OK)
+def delete_author(author_id: int, session: T_Session, user: T_CurrentUser):
+    db_author = session.scalar(
+        select(Author).where(Author.created_by_user == user.id, Author.id == author_id)
+    )
+
+    if not db_author:
+        raise HTTPException(
+            status_code=HTTPStatus.NOT_FOUND, detail='Author not found'
+        )
+
+    session.delete(db_author)
+    session.commit()
+
+    return {'message': 'Author deleted'}
---
+++ madr/routers/books.py
@@ -0,0 +1,105 @@
+from http import HTTPStatus
+from typing import Annotated
+
+from fastapi import APIRouter, Depends, Query
+from fastapi.exceptions import HTTPException
+from sqlalchemy import select
+from sqlalchemy.orm import Session
+
+from madr.database import get_session
+from madr.models import Book, User
+from madr.schemas import (
+    Message,
+    BookList,
+    BookPublic,
+    BookSchema,
+    BookUpdate,
+)
+from madr.security import get_current_user
+
+router = APIRouter(prefix='/books', tags=['books'])
+T_Session = Annotated[Session, Depends(get_session)]
+T_CurrentUser = Annotated[User, Depends(get_current_user)]
+
+
+@router.post('/', status_code=HTTPStatus.CREATED, response_model=BookPublic)
+def create_book(
+    user: T_CurrentUser,
+    book: BookSchema,
+    session: T_Session,
+):
+    db_book = Book(
+        title=book.title,
+        year=book.year,
+        user_id=user.id,
+        author_id=book.author_id
+    )
+    session.add(db_book)
+    session.commit()
+    session.refresh(db_book)
+
+    return db_book
+
+
+@router.get('/', response_model=BookList)
+def list_books(  # noqa
+    session: T_Session,
+    user: T_CurrentUser,
+    title: str = Query(None),
+    year: int = Query(None),
+    offset: int = Query(None),
+    limit: int = Query(None),
+):
+    query = select(Book).where(Book.user_id == user.id)
+
+    if title:
+        query = query.filter(Book.title.contains(title))
+
+    if year:
+        query = query.filter(Book.year == year)
+
+    books = session.scalars(query.offset(offset).limit(limit)).all()
+
+    return {'books': books}
+
+
+@router.patch(
+    '/{book_id}', status_code=HTTPStatus.OK, response_model=BookPublic
+)
+def update_book(
+    book_id: int, user: T_CurrentUser, session: T_Session, book: BookUpdate
+):
+    db_book = session.scalar(
+        select(Book).where(Book.user_id == user.id, Book.id == book_id)
+    )
+
+    if not db_book:
+        raise HTTPException(
+            status_code=HTTPStatus.NOT_FOUND, detail='Book not found'
+        )
+
+    for key, value in book.model_dump(exclude_unset=True).items():
+        setattr(db_book, key, value)
+
+    session.add(db_book)
+    session.commit()
+    session.refresh(db_book)
+
+    return db_book
+
+
+@router.delete('/{book_id}', response_model=Message, status_code=HTTPStatus.OK)
+def delete_book(book_id: int, session: T_Session, user: T_CurrentUser):
+    db_book = session.scalar(
+        select(Book).where(Book.user_id == user.id, Book.id == book_id)
+    )
+
+    if not db_book:
+        raise HTTPException(
+            status_code=HTTPStatus.NOT_FOUND, detail='Book not found'
+        )
+
+    session.delete(db_book)
+    session.commit()
+
+    return {'message': 'Book deleted'}
